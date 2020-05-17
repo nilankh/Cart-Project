5fc6109cd371ac66bcc33623a2226967e576f937
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-17 22:27:59 +0530

    added first step towards navbar

---
+++ src/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class NavBar extends React.Component {
+   
+    render () {
+        
+        return (
+            <div>
+                <div>
+                    <img src="https://image.flaticon.com/icons/svg/481/481384.svg" alt="cart-icon" />
+                    <span>3</span>
+                </div>
+            </div>
+            
+        );
+    }
+}
+
+
+export default NavBar;
