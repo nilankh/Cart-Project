f504c394653b468a497445a8ef8a3080c0dc0644
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-16 17:08:11 +0530

    fixed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ import './index.css';
 import App from './App';
 import * as firebase from 'firebase';
 import  'firebase/firestore';
-c
+const firebaseConfig = {
+  apiKey: "AIzaSyDsnE9EfuEh0EXx41oLNSaP5GlXaKRLdTg",
+  authDomain: "cart-dfd85.firebaseapp.com",
+  databaseURL: "https://cart-dfd85.firebaseio.com",
+  projectId: "cart-dfd85",
+  storageBucket: "cart-dfd85.appspot.com",
+  messagingSenderId: "729753412110",
+  appId: "1:729753412110:web:e7cbed445e9293afd7d25f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
